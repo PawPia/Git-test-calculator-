b4ed9c47513cb0fe2c9cc86240fd662e07e3d4bd
Pawel.Piasecki <250988@edu.p.lodz.pl>
2024-04-04 16:15:30 +0200

    adding a menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 
 int main() {
-    printf("Hello world !!!");
+    int option;
+    printf("Simple calculator\n\n");
+    printf("Pick an option: \n");
+    printf("1.Sum\n");
+    printf("2.Subtraction\n");
+    printf("3.Multiplication\n");
+    printf("4.Division\n");
+    printf("0. EXIT\n");
+    scanf("%d", &option);
+do{
+    switch(option){
+        case 1:
+            printf("###Sum###");
+            break;
+
+        case 2:
+            printf("###Subtraction###");
+            break;
+        case 3:
+            printf("###Multiplication###");
+            break;
+        case 4:
+            printf("###Division###");
+            break;
+        default:
+            printf("Pick a right option !!!\n\n");
+    }
+} while (option!= 0);
+printf("THE END !!!");
     return 0;
 }
